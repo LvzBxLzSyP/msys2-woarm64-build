16301706ba65873a1131de58c2b1960aba82dd69
LzSyp1923 <107896685+LvzBxLzSyP@users.noreply.github.com>
2025-09-27 13:44:10 +0800

    Update setup-mingwarm64.sh

--- .github/scripts/setup-mingwarm64.sh
+++ .github/scripts/setup-mingwarm64.sh
@@ -13,7 +13,7 @@ apply_patch () {
   if patch -R -p1 --dry-run -b -i "$1" > /dev/null 2>&1; then
     echo "Patch $1 is already applied"
   else
-    patch -p1 -b -i "$1"
+    git apply "$1"
   fi
 }
 
